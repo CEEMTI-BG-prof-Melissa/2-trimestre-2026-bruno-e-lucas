3d48a20b9ab3fdae7f7a2b03ec5026ed4ff5fb39
lucaslbmoura-stack <lucas.lbmoura@aluno.edu.es.gov.br>
2026-06-16 11:10:51 -0300

    Create index.php

---
+++ Aula 01 - php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html Lang "pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> Aula 01 html </title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+
+        ?>
+
+    </body>
+</html>
