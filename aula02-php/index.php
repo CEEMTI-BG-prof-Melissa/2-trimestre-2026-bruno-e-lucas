6081addd6e95bcdb27f628c5d6cbb9b9efcf1f7f
brunocmpaula-collab <bruno.cmpaula@aluno.edu.es.gov.br>
2026-06-24 10:59:58 -0300

    Create index.php

---
+++ aula02-php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta chaset="UTF-8">
+    <title>Aula 02</title>
+</head>
+<body>
+    <?php
+    $nome = "bruno";
+    $idade = 16;
+
+    echo"Meu nome é "  .$nome. " e minha idade  é " .$idade;
+    ?>
+</body>
+</html>
